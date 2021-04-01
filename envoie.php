09959f3a84d3564859ceb6c1027eda61433cd779
blierevan <71440952+blierevan@users.noreply.github.com>
2021-04-01 14:44:33 +0200

    Update envoie.php

--- envoie.php
+++ envoie.php
@@ -4,15 +4,10 @@ $mail=$HTTP_POST_VARS['mail'];
 $objet=$HTTP_POST_VARS['objet']; 
 $message=$HTTP_POST_VARS['message']; 
 
-/////voici la version Mine 
+
 $headers = "MIME-Version: 1.0\r\n"; 
-
-//////ici on détermine le mail en format text 
 $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n"; 
-
-////ici on détermine l'expediteur et l'adresse de réponse 
 $headers .= "From: $nom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP"; 
-
 $subject="$objet"; 
 $destinataire="contact@vitotel.fr"; 
 $body="$message"; 
